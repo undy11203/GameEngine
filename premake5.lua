d263b7914d1989167653485aa05d1e2bfd1e1e68
d.shabanov1 <d.shabanov1@g.nsu.ru>
2026-01-04 15:10:57 +0600

    add premake

---
+++ premake5.lua
@@ -0,0 +1,111 @@
+workspace "GameEngine"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS",
+			"GE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{MKDIR} ../bin/" .. outputdir .. "/Sandbox"),
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DSIT"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine/vendor/spdlog/include",
+		"GameEngine/src"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS"
+		}
+	
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DSIT"
+		optimize "On"
